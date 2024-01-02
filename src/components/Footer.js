8cda4474ecd54afc69d8552b2d33f923a80f295f
hadees621 <hadees621@gmail.com>
2024-01-02 17:29:19 +0500

    md screen done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,17 +23,17 @@ const Footer = () => {
 
 
     return (
-        <footer className="bg-[#0F1824]">
-            <div className="mx-auto w-full max-w-screen-2xl 320:py-10 py-32">
-                <div className="md:flex md:justify-between md:items-start">
+        <footer className="bg-[#0F1824] border">
+            <div className="mx-auto w-full max-w-screen-2xl 320:py-10 py-32 1440:text-center border justify-center items-center flex flex-col">
+                <div className="1440:grid 1440:grid-cols-2 border 1440:w-max">
 
-                    <div className="grid grid-cols-1 320:gap-12 320:justify-items-center">
+                    <div className="grid grid-cols-1 768:grid-cols-3 320:gap-12 320:justify-items-center border">
                         {renderList("Useful Links", usefulLinks)}
                         {renderList("Browsers", browsers)}
                         {renderList("Imprints", imprints)}
                     </div>
-                    
-                    <div className="flex flex-col justify-center 320:items-center 320:mt-8">
+
+                    <div className="flex flex-col 320:items-center 320:mt-8 border">
                         <p className="text-[#B6B4B9] mb-4 mx-1 text-[20px]">Follow us </p>
                         <div class="flex mt-4 sm:justify-center sm:mt-0 gap-10">
                             {FoooterSocialMediaLinks.map((link, index) => (
@@ -48,32 +48,28 @@ const Footer = () => {
 
                 </div>
 
-                <div className="md:flex md:justify-between md:items-start mt-10">
-                    <div className="flex flex-col">
-                        <p className="text-[#B6B4B9] mb-4 ml-1 text-lg">Sheraton House, Castle Park, Cambridge, CB3 0AX, United Kingdom
+                <div className="mt-10 px-2 1440:flex-row">
+                    <div className="w-full flex flex-col gap-5 320:text-center">
+                        <p className="text-[#B6B4B9] text-lg">Sheraton House, Castle Park, Cambridge, CB3 0AX, United Kingdom
                         </p>
-                        <p className="text-[#B6B4B9] mb-4 ml-1 text-lg">T: 01223 370012 | F: 01223 370040 | E: editors@pegasuspublishers.com
+                        <p className="text-[#B6B4B9] text-lg">T: 01223 370012 | F: 01223 370040 | E: editors@pegasuspublishers.com
                         </p>
                     </div>
-                    <div class="sm:flex sm:items-center sm:justify-between">
-                        <div className="flex mt-4 sm:justify-center sm:mt-0 gap-7">
-                            <img src="assests\footer\logo.jpg" alt="First Logo" className="w-20 h-20" />
-                            <img src="assests\footer\logo.jpg" alt="Second Logo" className="w-20 h-20" />
-                        </div>
+                    <div className="flex flex-col gap-6 py-5 320:items-center 768:flex-row 768:justify-center">
+                        <img src="assests\footer\logo.jpg" alt="First Logo" className="w-40 h-24" />
+                        <img src="assests\footer\logo.jpg" alt="Second Logo" className="w-40 h-24 " />
                     </div>
                 </div>
-
-
                 {/* line */}
-                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <hr className="my-9 border-gray-200 lg:my-8" />
 
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <div className="text-sm text-[#B6B4B9] sm:text-center ml-2">
+                <div className="flex flex-col gap-10 1440:flex-row">
+                    <div className="text-[15px] text-[#B6B4B9] text-center">
                         ©  1999 - 2020 Pegasus Elliot Mackenzie Publishers Ltd
                     </div>
-                    <div className="flex">
+                    <div className="flex 320:flex-col text-center">
                         {belowFooter.map((item, index) => (
-                            <li key={index} className="mb-4">
+                            <li key={index} className="mb-4 list-none">
                                 <a href="#" className="text-white text-lg">
                                     {item}
                                 </a>
