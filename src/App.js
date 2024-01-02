b2f1a6d7b79f56891e4c66a4ea5fbe1230526c8a
hadees621 <hadees621@gmail.com>
2024-01-02 10:29:49 +0500

    react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-80 text-lg">
+      Hello, Tailwind CSS!
+    </div>
+  );
+}
+
+export default App;
