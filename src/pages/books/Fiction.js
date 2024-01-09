164aeb2af442e1b550d4aabeb05f55bd67044795
hadees621 <hadees621@gmail.com>
2024-01-09 17:20:25 +0500

    route and page added

---
+++ src/pages/books/Fiction.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+
+const Fiction = () => {
+    return (
+        <>
+            {/* <Header /> */}
+            <div className="2560:mb-96 md:mb-48 320:mb-20">
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default Fiction;
