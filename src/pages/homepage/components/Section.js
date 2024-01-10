fd256cb5abe891e924755fc0c709fc0afc8ddff5
hadees621 <hadees621@gmail.com>
2024-01-10 18:28:00 +0500

    authors

--- src/pages/homepage/components/Section.js
+++ src/pages/homepage/components/Section.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { aboutAuthorData, articleData, audioBooksData, bookData, fantasyReadData, imprintData, provkingNovelData } from './../utils/utils'
 
 const ImprintSection = ({ title, content, imageSrc }) => (
-    <div className="1024:m-0 1024:py-4 1024:px-2 1024:h-[205px] 1440:m-4 m-4 p-10 320:p-6 768:p-8 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] h-[400px]">
-        <div className="w-full ">
-            <img className="h-[100px] 1024:h-[40px]" src={imageSrc} alt="Section Image" />
+    <div className="1024:m-0 1024:py-4 1024:px-2 1440:px-4 1440:m-0 2560:px-6 m-4 p-10 320:p-6 768:p-8 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] 2560:h-[380px] 1024:h-[205px] 1440:h-[235px]">
+        <div className="w-full">
+            <img className="2560:h-[100px] 1024:h-[40px] 2560:p-2" src={imageSrc} alt="Section Image" />
         </div>
         <div className="w-full text-start">
-            <h3 className="1440:text-3xl text-3xl 1024:text-[12px] font-bold font-fairplay my-3 1024:pt-0 pl-0">{title}</h3>
-            <p className="1440:text-base text-base 1024:text-[7px] text-[#2A2E30] font-roboto py-6 1024:py-0 tracking-normal leading-4">{content}</p>
+            <h3 className="1440:text-xl 2560:text-3xl 1024:text-[12px] font-bold font-fairplay my-3 1024:pt-0 pl-0">{title}</h3>
+            <p className="1440:text-[9px] 2560:text-base 1024:text-[7px] text-[#2A2E30] font-roboto  py-6 1024:py-0 tracking-normal 2560:leading-8 leading-4">{content}</p>
         </div>
     </div>
 );
@@ -106,7 +106,7 @@ const Section = ({ isMegaMenuOpen, applyBlur }) => {
                                 </div>
                                 <div className='w-full text-start items-end justify-end'>
                                     <h3 className="2560:text-3xl 2560:mt-4 font-bold font-fairplay 1024:text-[11px] 1440:text-[18px] 1440:mt-2">{article.title}</h3>
-                                    <p className="2560:text-[20px] 2560:mt-6 2560:leading-[30px] text-[#0E172B] font-roboto 1024:text-[6px] 1440:text-[9px] 1440:mt-2 1440:leading-[17px] leading-[14px]">{article.content}</p>
+                                    <p className="2560:text-[17.5px] 2560:mt-6 2560:leading-[30px] text-[#0E172B] font-roboto 1024:text-[6px] 1440:text-[9px] 1440:mt-2 1440:leading-[17px] leading-[14px]">{article.content}</p>
                                     <a href="#" className="text-[#0E172B] 2560:text-lg 1024:text-[6px] 1440:text-[10px] font-roboto tracking-wide underline border-[#5D6162] items-start inline-block 1440:mt-6 2560:mt-9">Read more</a>
                                 </div>
                             </div>
@@ -137,12 +137,12 @@ const Section = ({ isMegaMenuOpen, applyBlur }) => {
                     </p>
 
                     {/* imprints */}
-                    <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-4 mt-8 gap-1'>
-                        <div className="1024:m-1 1440:m-4 m-4 p-4 1024:p-3 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] h-[400px] 1024:h-[205px]">
-                            <div className='w-full text-start mt-10 1024:mt-4'>
-                                <h3 className="text-3xl text-[#5D6162] font-fairplay pt-4 1024:pt-3 1024:text-[12px]">Our imprints</h3>
-                                <p className="text-sm text-[#5D6162] font-roboto py-4 pr-4 text-start 1024:text-[6px] 1024:leading-3 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                <button className="mt-6 1024:mt-0 bg-[#031428] text-lg text-white px-4 py-2 rounded-3xl w-[150px] h-12 1024:w-[70px] 1024:h-[20px] 1024:text-[7px] flex justify-center items-center font-fairplay">Explore</button>
+                    <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-4 mt-8 gap-1 1440:gap-2'>
+                        <div className="1024:m-0 1024:py-4 1024:px-2 1440:px-4 1440:m-0 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] 2560:h-[380px] 1024:h-[205px] 1440:h-[235px]">
+                            <div className='w-full text-start 2560:mt-10 1024:mt-4'>
+                                <h3 className="2560:text-3xl text-[#5D6162] font-fairplay 2560:pt-8 1024:pt-3 1024:text-[12px] 1440:pt-5 1440:text-[18px]">Our imprints</h3>
+                                <p className="2560:text-sm text-[#5D6162] font-roboto py-6 pr-4 text-start 1024:text-[6px] 1024:leading-3 1440:text-[8px] 1440:leading-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <button className="2560:mt-6 1024:mt-0 bg-[#031428] 2560:text-lg text-white 2560:px-4 2560:py-2 rounded-3xl 2560:w-[150px] 2560:h-12 1024:w-[70px] 1024:h-[20px] 1024:text-[7px] 1440:w-[80px] 1440:h-[25px] 1440:text-[10px] flex justify-center items-center font-fairplay">Explore</button>
                             </div>
                         </div>
 
@@ -152,76 +152,76 @@ const Section = ({ isMegaMenuOpen, applyBlur }) => {
                     </div>
 
                     {/* Thoughts provoking  */}
-                    <div className="flex items-center mt-6">
+                    <div className="flex items-center mt-10">
                         <div className="flex-grow border-t border-[#8F9191]"></div>
-                        <div className="text-[#5D6162] 320:text-lg 1024:text-[11px] font-fairplay px-2">Thought provoking novels</div>
+                        <div className="text-[#5D6162] 320:text-lg 1024:text-[11px] 1440:text-[15px] 2560:text-[28px] font-fairplay px-2">Thought provoking novels</div>
                         <div className="flex-grow border-t border-[#8F9191]"></div>
                     </div>
 
-                    <div className="grid 768:grid-cols-3 1024:grid-cols-6 justify-center">
+                    <div className="grid 768:grid-cols-3 1024:grid-cols-6 justify-center 2560:mt-6">
                         {provkingNovelData.map((book, index) => (
                             <div key={index} className="m-4 p-3 text-center 768:m-0 1440:m-1">
-                                <img className="320:h-[420px] 1024:h-[140px] 1024:w-[160px] 2560:h-[440px]" src={book.imageSrc} alt={`Book Image ${index + 1}`} />
+                                <img className="320:h-[420px] 1024:h-[140px] 1024:w-[160px] 1440:h-[210px] 2560:w-[240px] 2560:h-[380px]" src={book.imageSrc} alt={`Book Image ${index + 1}`} />
                                 <div className='w-full text-start my-2'>
-                                    <h3 className="text-[8px] font-medium font-fairplay">{book.title}</h3>
-                                    <p className="text-[6px] text-[#B79248] font-roboto">{book.author}</p>
+                                    <h3 className="1024:text-[8px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay">{book.title}</h3>
+                                    <p className="1024:text-[6px] 1440:text-[9px] 2560:text-[17px] text-[#B79248] font-roboto">{book.author}</p>
                                 </div>
                             </div>
                         ))}
                     </div>
 
                     <p className="text-center">
-                        <a href="#" className="text-[#5D6162] text-[6px] font-fairplay tracking-wide inline-block pb-[2px] border-b border-[#5D6162]">Browse more</a>
+                        <a href="#" className="text-[#5D6162] 1024:text-[6px] 1440:text-[11px] 2560:text-[21px] 2560:mt-6 font-fairplay tracking-wide inline-block pb-[2px] border-b border-[#5D6162]">Browse more</a>
                     </p>
 
                     {/* <div className="flex items-center py-8 mt-6">
-                    <div className="flex-grow border-t border-[#8F9191]"></div>
-                    <div className="text-[#5D6162] 320:text-lg 1024:text-2xl font-fairplay px-4">Author interviews
+                        <div className="flex-grow border-t border-[#8F9191]"></div>
+                        <div className="text-[#5D6162] 320:text-lg 1024:text-2xl font-fairplay px-4">Author interviews
+                        </div>
+                        <div className="flex-grow border-t border-[#8F9191]"></div>
                     </div>
-                    <div className="flex-grow border-t border-[#8F9191]"></div>
-                </div>
 
-                <div className='flex bg-[#0F1824] h-[730px] justify-center items-center px-10'>
+                    <div className='flex bg-[#0F1824] h-[730px] justify-center items-center px-10'>
 
-                    <div className='w-1/3 h-[640px]'>
-                        <div className='w-full text-start mt-10 py-8 pr-4'>
-                            <h3 className="text-3xl font-fairplay pt-4 text-[#B79248]">Francis James | Author Interview for
-                                The Mad Hatter's Tea Party
-                            </h3>
-                            <p className="text-lg text-white font-roboto py-4 pr-10 text-start">This is a novel about relationships, fears and feelings as it
-                                traverses the sometimes long road to good mental health. A
-                                must read for everyone interested in mental illness - that is, all
-                                of us!</p>
-                            <button className="mt-6 bg-white text-lg text-black px-4 py-2 rounded-3xl w-[150px] h-12 font-fairplay mb-4">View more</button>
-                            <div className='flex gap-8 m-8'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><path fill="none" stroke="white" stroke-width="2" d="m7 2l10 10L7 22" /></g></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-width="2" d="m7 2l10 10L7 22" /></svg>
-                                <p className='text-white'>
-                                    01 / 03
+                        <div className='w-1/3 h-[640px]'>
+                            <div className='w-full text-start mt-10 py-8 pr-4'>
+                                <h3 className="text-3xl font-fairplay pt-4 text-[#B79248]">Francis James | Author Interview for
+                                    The Mad Hatter's Tea Party
+                                </h3>
+                                <p className="text-lg text-white font-roboto py-4 pr-10 text-start">This is a novel about relationships, fears and feelings as it
+                                    traverses the sometimes long road to good mental health. A
+                                    must read for everyone interested in mental illness - that is, all
+                                    of us!</p>
+                                <button className="mt-6 bg-white text-lg text-black px-4 py-2 rounded-3xl w-[150px] h-12 font-fairplay mb-4">View more</button>
+                                <div className='flex gap-8 m-8'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><path fill="none" stroke="white" stroke-width="2" d="m7 2l10 10L7 22" /></g></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-width="2" d="m7 2l10 10L7 22" /></svg>
+                                    <p className='text-white'>
+                                        01 / 03
 
-                                </p>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='w-2/3 h-[640px]'>
-                        <img className="object-cover" src="assests/section/youtube.png" alt={`Book Image`} />
-                    </div>
-                </div> */}
+                        <div className='w-2/3 h-[640px]'>
+                            <img className="object-cover" src="assests/section/youtube.png" alt={`Book Image`} />
+                        </div>
+                    </div> */}
 
                     {/* about author */}
                     <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 mt-8 px-20 320:px-0 gap-4 768:p-10 1024:p-0 1024:px-0 1024:gap-0'>
                         {aboutAuthorData.map((author, index) => (
                             <div
                                 key={index}
-                                className="m-4 320:m-0 320:p-3 768:p-10 1024:p-1 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] h-[450px] 1024:h-[230px] 1440:h-[520px] 2560:h-[450px]"
+                                className="m-4 320:m-0 320:p-3 768:p-10 1024:p-1 1440:p-3 text-center group bg-white transition-colors duration-300 ease-in-out hover:bg-[#EDEFF1] h-[450px] 1024:h-[230px] 1440:h-[270px] 2560:h-[450px]"
                             >
-                                <div className="w-full text-start p-10 320:p-0 1024:p-3">
-                                    <h3 className="text-2xl font-normal font-fairplay text-[#B79248] 1024:text-[10px] underline">{author.name}</h3>
-                                    <h3 className="text-2xl font-normal font-fairplay pt-1 text-[#B79248] underline 1024:text-[10px] 1024:pt-0">{author.title}</h3>
-                                    <p className="text-base text-[#2A2E30] font-fairplay py-6 tracking-normal 1024:text-[6px] 1024:py-2">{author.message}</p>
-                                    <p className="text-base text-[#2A2E30] font-fairplay py-4 tracking-normal 1024:text-[7px] 1024:leading-3 1024:py-1">{author.paragraph}</p>
+                                <div className="w-full text-start 2560:p-10 320:p-0 1024:p-3">
+                                    <h3 className="2560:text-2xl font-normal font-fairplay text-[#B79248] 1024:text-[10px] 1440:text-[18px] underline">{author.name}</h3>
+                                    <h3 className="2560:text-2xl font-normal font-fairplay 1440:p-1 2560:pt-1 text-[#B79248] underline 1024:text-[18px] 1024:pt-0">{author.title}</h3>
+                                    <p className="2560:text-base text-[#2A2E30] font-fairplay 1440:py-4 2560:py-6 tracking-normal 1024:text-[6px] 1440:text-[12px] 1024:py-2">{author.message}</p>
+                                    <p className="2560:text-base text-[#2A2E30] font-fairplay 1440:py-2 2560:py-4 tracking-normal 1024:text-[7px] 1440:text-[10px] 1024:leading-3 1024:py-1">{author.paragraph}</p>
                                     {author.button && (
-                                        <button className="mt-6 bg-[#031428] text-lg text-white px-4 py-2 rounded-3xl w-[150px] h-12 font-fairplay 1024:w-[63px] 1024:h-[20px] 1024:text-[7px] flex justify-center items-center">
+                                        <button className="2560:mt-6 bg-[#031428] 2560:text-lg text-white px-4 py-2 rounded-3xl 2560:w-[150px] 2560:h-12 font-fairplay 1024:w-[63px] 1024:h-[20px] 1024:text-[7px] 1440:w-[73px] 1440:h-[25px] 1440:text-[9px] 1440:my-6 flex justify-center items-center">
                                             Read more
                                         </button>
                                     )}
