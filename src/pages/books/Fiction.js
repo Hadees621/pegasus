414f793e1b5d7dc9235a5055fecdf1864f8d2f4c
hadees621 <hadees621@gmail.com>
2024-01-12 17:55:35 +0500

    resp done

--- src/pages/books/Fiction.js
+++ src/pages/books/Fiction.js
@@ -76,24 +76,26 @@ const Fiction = () => {
         <>
             <Header />
             <>
-                <div className="bg-[#F1F1F3] justify-center 320:m-5 1024:mx-48 2560:mx-96 2560:h-96 1024:h-52 1440:h-72 border 1440:flex-row xl:mx-80 ">
-                    <p className="text-sm font-roboto m-10 2560:m-10 1024:text-[8px] 1024:m-4 1440:text-[16px] 1440:m-6">Books — Fiction</p>
+                <div className="bg-[#F1F1F3] justify-center 320:m-5 1024:h-52 1440:h-72 2560:h-[420px] 1440:flex-row 1024:mx-40 xl:mx-52 2xl:mx-96 border border-[#F1F1F3]">
+                    <p className="2560:text-sm font-roboto m-10 2560:m-10 1024:text-[8px] 1024:m-4 1440:text-[9px] 1440:m-5">Books — Fiction</p>
                     <div className="flex w-full justify-center">
-                        <p className="2560:text-7xl 320:text-4xl 768:text-5xl 1024:text-[30px] flex justify-center font-fairplay my-24 1024:my-10 border-b-2 border-black shadow-sm w-max">Fiction</p>
+                        <p className=" 320:text-4xl 768:text-5xl 1024:text-[30px] 1440:text-[45px] 2560:text-7xl flex justify-center font-fairplay my-24 1024:my-10 border-b-2 border-black shadow-sm w-max">Fiction</p>
                     </div>
                 </div>
 
-                <div className="flex 320:flex-col 320:items-center 320:gap-10 1024:flex-row 1440:flex-row border 320:mx-5 2560:mx-96 1024:mt-0 1024:gap-2 1024:mx-48 1440:mt-0 mt-12 1440:gap-4  1440:mx-48 xl:mx-80">
+                <div className="flex 320:flex-col 320:items-center 320:gap-10 1024:flex-row 1440:flex-row 320:mx-5 1024:mt-0 1024:gap-2 1440:mt-0 mt-12 1024:mx-40 xl:mx-52 2xl:mx-96 ">
 
-                    <div className="h-16 w-[15%] 320:w-full 320:justify-center 1440:w-[25%] border-black flex items-center 1440:justify-start gap-2 border">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2M6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2z" /></svg>
-                        <p className="text-xl 1440:text-sm font-fairplay">Filter by</p>
+                    <div className="2560:h-16 2560:w-[15%] 320:w-full 320:justify-center 1024:w-[25%] 1440:w-[30%] border-black flex items-center 1024:justify-start gap-2 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="1024:w-4">
+                            <path fill="currentColor" d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2M6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2z" />
+                        </svg>
+                        <p className="text-xl 1024:text-xs 1440:text-[15px] 2560:text-xl font-fairplay">Filter by</p>
                     </div>
 
                     {/* rounds */}
-                    <div className="flex 320:flex-col 1440:flex-row 1440:w-[75%] items-center justify-start h-16 w-[85%] border-black gap-4 px-8 1440:gap-1 1440:px-2 border">
-                        <button className="flex items-center justify-center gap-2 border w-max px-5 py-2.5  1440:px-3 1440:py-1 rounded-full border-[#D4D4D8]">
-                            <p className="text-[17px] 1440:text-[12px] font-fairplay">Explore</p>
+                    <div className="flex 320:flex-col 320:gap-6 1024:flex-row 1024:gap-1 1024:w-[75%] 1440:w-[70%] items-center justify-start 2560:h-16 2560:w-[85%] border-black 2560:gap-4 2560:px-8 1440:gap-1 1024:px-4 ">
+                        <button className="flex items-center justify-center gap-2 border w-max px-8 py-2.5 1024:px-2 1024:py-0.5 1440:px-4 1440:py-1.5  2560:px-8 2560:py-2.5 rounded-full border-[#D4D4D8]">
+                            <p className="text-[17px] 1024:text-[10px] 1440:text-[14px] 2560:text-[20px] font-fairplay">Explore</p>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -102,13 +104,13 @@ const Fiction = () => {
                                 strokeLinejoin="round"
                                 strokeWidth="2"
                                 viewBox="0 0 24 24"
-                                className="flex items-center justify-center gap-2 w-6 h-6 1440:w-4 text-gray-500 hover:text-gray-700 "
+                                className="flex items-center justify-center gap-2 w-6 h-6 1024:w-3 1440:w-4 2560:w-6 text-gray-500 hover:text-gray-700 "
                             >
                                 <path d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
-                        <button className="flex items-center justify-center gap-2 border w-max px-5 py-2.5  1440:px-3 1440:py-1 rounded-full border-[#D4D4D8]">
-                            <p className="text-[17px] 1440:text-[12px] font-fairplay">Vanguard press</p>
+                        <button className="flex items-center justify-center gap-2 border w-max px-8 py-2.5 1024:px-2 1024:py-0.5 1440:px-4 1440:py-1.5 2560:px-8 2560:py-2.5 rounded-full border-[#D4D4D8]">
+                            <p className="text-[17px] 1024:text-[10px] 1440:text-[14px]  2560:text-[20px] font-fairplay">Vanguard press</p>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -117,13 +119,13 @@ const Fiction = () => {
                                 strokeLinejoin="round"
                                 strokeWidth="2"
                                 viewBox="0 0 24 24"
-                                className="flex items-center justify-center gap-2 w-6 h-6 text-gray-500 hover:text-gray-700 "
+                                className="flex items-center justify-center gap-2 w-6 h-6 1024:w-3 1440:w-4 2560:w-6 text-gray-500 hover:text-gray-700 "
                             >
                                 <path d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
-                        <button className="flex items-center justify-center gap-2 border w-max px-5 py-2.5  1440:px-3 1440:py-1 rounded-full border-[#D4D4D8]">
-                            <p className="text-[17px] 1440:text-[12px] font-fairplay">Under £15</p>
+                        <button className="flex items-center justify-center gap-2 border w-max px-8 py-2.5 1024:px-2 1024:py-0.5 1440:px-4 1440:py-1.5 2560:px-8 2560:py-2.5 rounded-full border-[#D4D4D8]">
+                            <p className="text-[17px] 1024:text-[10px] 1440:text-[14px] 2560:text-[20px] font-fairplay">Under £15</p>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -132,19 +134,23 @@ const Fiction = () => {
                                 strokeLinejoin="round"
                                 strokeWidth="2"
                                 viewBox="0 0 24 24"
-                                className="flex items-center justify-center gap-2 w-6 h-6 text-gray-500 hover:text-gray-700 "
+                                className="flex items-center justify-center gap-2 w-6 h-6 1024:w-3 1440:w-4 2560:w-6 text-gray-500 hover:text-gray-700 "
                             >
                                 <path d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
                     </div>
 
-                    <div class="flex relative group 320:mt-32 320:mb-5 1440:mt-0 1440:mb-0 border" >
+                    <div class="flex relative group 320:mt-14 320:mb-5 1024:mt-0 1024:mb-0 ">
                         <div className="">
-                            <button className="flex items-center justify-center gap-2 w-max px-5 py-3 rounded-full border-[#D4D4D8]">
-                                <p className="text-[20px] font-fairplay">Sort by</p>
+                            <button className="flex items-center justify-center gap-2 w-max 1024:px-2 1024:py-1 px-5 py-3 rounded-full border-[#D4D4D8]">
+                                <p className="text-[20px] 1024:text-[13px] 1440:text-[15px] 2560:text-[22px] font-fairplay">Sort by</p>
                                 <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><g transform="rotate(180 12 12)"><path fill="currentColor" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" /></g></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" className="1024:w-5 1440:w-6 2560:w-8">
+                                        <g transform="rotate(180 12 12)">
+                                            <path fill="currentColor" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" />
+                                        </g>
+                                    </svg>
                                     <div />
                                 </div>
                             </button>
@@ -159,20 +165,19 @@ const Fiction = () => {
 
                 </div>
 
-                <hr className="border-1 border-black mb-2 mt-10" />
+                <hr className="border-1 border-[#EDEEEE] mb-2 mt-10" />
 
-
-                <div className="flex 320:flex-col 320:items-center 1024:mx-10 320:mx-5 1440:mx-36 2560:mx-96 gap-4">
-                    <div className="w-[15%] 320:w-full 320:my-6 border-black 320:px-6">
+                <div className="flex 320:flex-col 320:items-center 320:mx-5 gap-4 1024:flex-row 1024:items-start 1024:mx-40 1024:gap-2 xl:mx-52 2xl:mx-96">
+                    <div className="w-[15%] 320:w-full 320:my-6 border-black 320:px-6  1024:px-0 1024:my-0 1024:w-[25%] 1440:w-[30%] 2560:w-[15%] ">
                         <Sidebar categories={categories} />
                     </div>
-                    <div className="w-[85%] 320:w-full border-black ">
+                    <div className="w-[85%] 320:w-full border-black 1024:w-[75%] 1440:w-[70%] 2560:w-[85%] ">
                         <div className="grid 768:grid-cols-3 1024:grid-cols-5 justify-center">
                             {bookData.map((book, index) => (
                                 <div key={index} className="m-4 p-3 text-center 768:m-0 1440:m-1">
                                     <img className="320:h-[420px] 1024:h-[140px] 1024:w-[160px] 1440:h-[210px] 2560:w-[240px] 2560:h-[380px]" src={book.imageSrc} alt={`Book Image ${index + 1}`} />
-                                    <div className='w-full text-start my-2'>
-                                        <h3 className="1024:text-[8px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay">{book.title}</h3>
+                                    <div className='w-full text-start my-3'>
+                                        <h3 className="1024:text-[6px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay">{book.title}</h3>
                                         <p className="1024:text-[6px] 1440:text-[9px] 2560:text-[17px] text-[#B79248] font-roboto">{book.author}</p>
                                     </div>
                                 </div>
--- src/pages/books/components/SideBar.js
+++ src/pages/books/components/SideBar.js
@@ -95,28 +95,28 @@ const Sidebar = () => {
 
     return (
         <>
-            <div className='mb-6 px-2'>
+            <div className='mb-6 1024:mb-2 px-2 1024:px-1'>
                 <div className='mt-'>
                     {/* Categories */}
-                    <div className={`flex my-1 justify-between cursor-pointer`} onClick={toggleCategories}>
-                        <p className="text-[21px] font-fairplay text-black">{categoriesData[0].mainTitle}</p>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                    <div className={`flex my-1 items-center justify-between cursor-pointer`} onClick={toggleCategories}>
+                        <p className="text-[21px] font-fairplay text-black 1024:text-[12px] 1440:text-[15px] 2560:text-[21px]">{categoriesData[0].mainTitle}</p>
+                        <div className='flex items-center justify-center'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className='1024:w-4 1440:w-6 2560:w-8'>
                                 <g transform="rotate(180 12 12)">
                                     <path fill="Black" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" />
                                 </g>
                             </svg>
                         </div>
                     </div>
-                    <div className={`block ${categoriesOpen && isMediumScreen ? '' : 'hidden'}`}>
+                    <div className={`block ${categoriesOpen ? '' : 'hidden'}`}>
                         {categoriesData[0].items.map((category, index) => (
                             <div key={index}>
-                                <p className='mb-3 mt-5 text-black font-medium'>{category.title}</p>
+                                <p className='mb-3 mt-5 text-black font-medium 1024:text-[9px] 1440:text-[12px] 2560:text-[19px] 1024:mt-3 2560:mt-5'>{category.title}</p>
                                 <ul className="space-y-3">
                                     <li>
                                         <ul className="space-y-3">
                                             {category.items.map((item, i) => (
-                                                <li key={i} className='text-[#75797B]'>{item}</li>
+                                                <li key={i} className='text-[#75797B] 1024:text-[9px] 1440:text-[12px] 2560:text-[19px]'>{item}</li>
                                             ))}
                                         </ul>
                                     </li>
@@ -126,12 +126,14 @@ const Sidebar = () => {
                     </div>
                 </div>
 
-                <div className='mt-10'>
+                <hr className="border-1 border-[#EDEEEE] mt-8" />
+
+                <div className='mt-10 1024:mt-4 2560:mt-8'>
                     {/* Imprints */}
-                    <div className='flex my-3 justify-between cursor-pointer' onClick={toggleImprints}>
-                        <p className="text-[21px] font-fairplay text-black">{categoriesData[1].mainTitle}</p>
+                    <div className='flex my-3 1024:my-0 items-center justify-between cursor-pointer' onClick={toggleImprints}>
+                        <p className="text-[21px] font-fairplay text-black 1024:text-[12px] 1440:text-[15px] 2560:text-[21px]">{categoriesData[1].mainTitle}</p>
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className='1024:w-4 1440:w-6 2560:w-8'>
                                 <g transform="rotate(180 12 12)">
                                     <path fill="Black" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" />
                                 </g>
@@ -144,7 +146,7 @@ const Sidebar = () => {
                             <li>
                                 <ul className="space-y-3">
                                     {categoriesData[1].items.map((item, i) => (
-                                        <li key={i} className='text-[#75797B]'>{item}</li>
+                                        <li key={i} className='text-[#75797B] 1024:text-[9px] 1440:text-[12px] 2560:text-[19px]'>{item}</li>
                                     ))}
                                 </ul>
                             </li>
@@ -152,12 +154,14 @@ const Sidebar = () => {
                     </div>
                 </div>
 
-                <div className='mt-10'>
+                <hr className="border-1 border-[#EDEEEE] mt-8" />
+
+                <div className='mt-10 1024:mt-4 2560:mt-8'>
                     {/* Formats */}
-                    <div className='flex my-3 justify-between cursor-pointer' onClick={toggleFormats}>
-                        <p className="text-[21px] font-fairplay text-black">{categoriesData[2].mainTitle}</p>
+                    <div className='flex my-3 1024:my-0 items-center justify-between cursor-pointer' onClick={toggleFormats}>
+                        <p className="text-[21px] font-fairplay text-black 1024:text-[12px] 1440:text-[15px] 2560:text-[21px]">{categoriesData[2].mainTitle}</p>
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className='1024:w-4 1440:w-6  2560:w-8'>
                                 <g transform="rotate(180 12 12)">
                                     <path fill="Black" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" />
                                 </g>
@@ -170,7 +174,7 @@ const Sidebar = () => {
                             <li>
                                 <ul className="space-y-3">
                                     {categoriesData[2].items.map((item, i) => (
-                                        <li key={i} className='text-[#75797B]'>{item}</li>
+                                        <li key={i} className='text-[#75797B] 1024:text-[9px] 1440:text-[12px] 2560:text-[19px]'>{item}</li>
                                     ))}
                                 </ul>
                             </li>
