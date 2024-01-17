645108fadbcded09e4e5ae840ca290d084165332
hadees621 <hadees621@gmail.com>
2024-01-17 17:08:42 +0500

    resp all

--- src/pages/features/Features.js
+++ src/pages/features/Features.js
@@ -44,19 +44,19 @@ const Features = () => {
                 </div>
 
                 {/* filter  */}
-                <div className="flex 320:flex-col 320:items-center 320:gap- 1024:flex-row 1440:flex-row 320:mx-5 1024:mt-0 1024:gap-2 1440:mt-0 mt-12 1024:mx-40 xl:mx-52 2xl:mx-96 justify-between 2000:py-10">
+                <div className="flex 320:flex-col 320:items-center 320:gap- 1024:flex-row 1440:flex-row 320:mx-5 1024:mt-0 1024:gap-2 1440:mt-0 mt-12 1024:mx-40 xl:mx-52 2xl:mx-96 justify-between 2000:py-1">
 
                     <div className="2000:h-16 2000:w-[15%] 320:w-full 320:justify-center 1024:w-[25%] 1440:w-[30%] border-black flex items-center 1024:justify-start gap-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="1024:w-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="1024:w-2 2000:w-6">
                             <path fill="currentColor" d="M9 5a1 1 0 1 0 0 2a1 1 0 0 0 0-2M6.17 5a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-7.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM15 11a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 0 1 0-2zM9 17a1 1 0 1 0 0 2a1 1 0 0 0 0-2m-2.83 0a3.001 3.001 0 0 1 5.66 0H19a1 1 0 1 1 0 2h-1.17a3.001 3.001 0 0 1-5.66 0H5a1 1 0 1 1 0-2z" />
                         </svg>
-                        <p className="text-xl 1024:text-[8px] 1440:text-[15px] 2000:text-xl font-fairplay">Filter by</p>
+                        <p className="text-xl 1024:text-[8px] 1440:text-[15px] 2000:text-[21px] font-fairplay">Filter by</p>
                     </div>
 
                     <div class="flex relative group 320:mt-14 320:mb-5 1024:mt-0 1024:mb-0 ">
                         <div className="">
                             <button className="flex items-center justify-center gap-1 w-max 1024:px-0 1024:py-1 px-5 py-3 rounded-full border-[#D4D4D8]">
-                                <p className="text-[20px] 1024:text-[8px] 1440:text-[15px] 2000:text-[22px] font-fairplay">Sort by</p>
+                                <p className="text-[20px] 1024:text-[8px] 1440:text-[15px] 2000:text-[21px] font-fairplay">Sort by</p>
                                 <div>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" className="1024:w-3 1440:w-6 2000:w-8">
                                         <g transform="rotate(180 12 12)">
@@ -79,16 +79,16 @@ const Features = () => {
 
                 {/* blogs */}
                 <div className="flex 320:flex-col 320:items-center 320:mx-5 gap-4 1024:flex-row 1024:items-start 1024:mx-40 1024:gap-2 xl:mx-52 2xl:mx-96">
-                    <div className="w-[15%] 320:w-full 320:my-6 border-black 320:px-6 1024:px-0 1024:my-0 1024:w-[17%] 1440:w-[17%] 2000:w-[15%] 1440:mt-4">
+                    <div className="w-[15%] 320:w-full 320:my-6 border-black 320:px-6 1024:px-0 1024:my-0 1024:w-[17%] 1440:w-[17%] 2000:w-[18%] 1440:mt-4 2000:mt-6">
                         <Sidebar />
                     </div>
-                    <div className="w-[85%] 320:w-full border-black 1024:w-[90%] 1440:w-[90%] 2000:w-[85%]">
+                    <div className="w-[85%] 320:w-full border-black 1024:w-[90%] 1440:w-[90%] 2000:w-[95%]">
                         <div className="grid justify-center">
 
                             {/* blog category */}
                             <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 320:gap-4 1024:gap-6 mt-8 1024:mt-0 1440:mt-4 2000:mt-10'>
                                 {articleData.map((article, index) => (
-                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2">
+                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2 2000:space-y-7">
                                         <div className='flex justify-between'>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.category}</h3>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.date}</h3>
@@ -96,7 +96,7 @@ const Features = () => {
                                         <div className='w-full'>
                                             <img className="w-full object-cover" src="assests/features/art.png" alt={`Section ${index + 1} Image`} />
                                         </div>
-                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1'>
+                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1 2000:space-y-3'>
                                             <h3 className="768:text-[20px] 2000:text-3xl 2000:mt-4 font-bold font-fairplay 1024:text-[11px] 1440:text-[18px] 1440:mt-2 text-[#0F1824]">{article.title}</h3>
                                             <p className="768:text-[17px] 768:leading-[20px] 1024:leading-[12px] 2000:text-[17.5px] 2000:mt-6 2000:leading-[30px] text-[#0F1824] font-roboto 320:text-[12px] 1024:text-[6px] 1440:text-[9px] 1440:mt-2 1440:leading-[17px] leading-[14px]">{article.content}</p>
 
@@ -108,9 +108,9 @@ const Features = () => {
                         <div className="grid justify-center">
 
                             {/* blog category */}
-                            <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 320:gap-4 1024:gap-6 mt-8 1024:mt-0 1440:mt-6 2000:mt-10'>
+                            <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 320:gap-4 1024:gap-6 mt-8 1024:mt-0 1440:mt-4 2000:mt-10'>
                                 {articleData.map((article, index) => (
-                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2">
+                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2 2000:space-y-7">
                                         <div className='flex justify-between'>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.category}</h3>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.date}</h3>
@@ -118,7 +118,7 @@ const Features = () => {
                                         <div className='w-full'>
                                             <img className="w-full object-cover" src="assests/features/art.png" alt={`Section ${index + 1} Image`} />
                                         </div>
-                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1'>
+                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1 2000:space-y-3'>
                                             <h3 className="768:text-[20px] 2000:text-3xl 2000:mt-4 font-bold font-fairplay 1024:text-[11px] 1440:text-[18px] 1440:mt-2 text-[#0F1824]">{article.title}</h3>
                                             <p className="768:text-[17px] 768:leading-[20px] 1024:leading-[12px] 2000:text-[17.5px] 2000:mt-6 2000:leading-[30px] text-[#0F1824] font-roboto 320:text-[12px] 1024:text-[6px] 1440:text-[9px] 1440:mt-2 1440:leading-[17px] leading-[14px]">{article.content}</p>
 
@@ -130,9 +130,9 @@ const Features = () => {
                         <div className="grid justify-center">
 
                             {/* blog category */}
-                            <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 320:gap-4 1024:gap-6 mt-8 1024:mt-0 1440:mt-6 2000:mt-10'>
+                            <div className='grid grid-cols-3 320:grid-cols-1 1024:grid-cols-3 320:gap-4 1024:gap-6 mt-8 1024:mt-0 1440:mt-4 2000:mt-10'>
                                 {articleData.map((article, index) => (
-                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2">
+                                    <div key={index} className="m-3 text-center 1024:m-2 320:space-y-5 1024:space-y-2 2000:space-y-7">
                                         <div className='flex justify-between'>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.category}</h3>
                                             <h3 className="2000:text-lg 1024:text-[6px] 1440:text-[8px] font-fairplay text-[#45484A]">{article.date}</h3>
@@ -140,7 +140,7 @@ const Features = () => {
                                         <div className='w-full'>
                                             <img className="w-full object-cover" src="assests/features/art.png" alt={`Section ${index + 1} Image`} />
                                         </div>
-                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1'>
+                                        <div className='w-full text-start items-end justify-end 320:space-y-8 1024:space-y-1 2000:space-y-3'>
                                             <h3 className="768:text-[20px] 2000:text-3xl 2000:mt-4 font-bold font-fairplay 1024:text-[11px] 1440:text-[18px] 1440:mt-2 text-[#0F1824]">{article.title}</h3>
                                             <p className="768:text-[17px] 768:leading-[20px] 1024:leading-[12px] 2000:text-[17.5px] 2000:mt-6 2000:leading-[30px] text-[#0F1824] font-roboto 320:text-[12px] 1024:text-[6px] 1440:text-[9px] 1440:mt-2 1440:leading-[17px] leading-[14px]">{article.content}</p>
 
@@ -155,11 +155,11 @@ const Features = () => {
                 {/* pagination */}
                 <div className="flex flex-col gap-4 768:gap-8 justify-center items-center h-20 my-10 lg:flex-row lg:justify-between 1024:mx-44 xl:mx-52 2xl:mx-96">
                     <nav className="lg:mx-auto">
-                        <ul class="flex items-center h-8 space-x-4 1024:text-[8px] 1024:space-x-1.5 1440:text-[10px] 1440:space-x-2">
+                        <ul class="flex items-center h-8 space-x-4 1024:text-[8px] 2000:text-[22px] 1024:space-x-1.5 1440:text-[10px] 1440:space-x-2 2000:space-x-4">
                             <li>
                                 <a href="#" class="flex items-center justify-center h-8 ms-0 leading-tight text-gray-500 bg-white ">
                                     <span class="sr-only">Previous</span>
-                                    <svg class="w-2.5 h-2.5 1024:w-1 1440:w-[4px] rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <svg class="w-2.5 h-2.5 1024:w-1 1440:w-[4px] 2000:w-[20px] 2000:h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                                     </svg>
                                 </a>
@@ -182,7 +182,7 @@ const Features = () => {
                             <li>
                                 <a href="#" class="flex items-center justify-center h-8 leading-tight text-gray-500">
                                     <span class="sr-only">Next</span>
-                                    <svg class="w-2.5 h-2.5 1024:w-1 1440:w-[4px] rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                    <svg class="w-2.5 h-2.5 1024:w-1 1440:w-[4px] 2000:w-[20px] 2000:h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                                     </svg>
                                 </a>
@@ -191,7 +191,7 @@ const Features = () => {
                     </nav>
 
                     <p className="flex lg:justify-end">
-                        <a href="#" className="text-[20px] 1024:text-[7px] 1440:text-[10px] text-[#5D6162] font-fairplay tracking-wide inline-block pb-[2px] border-b border-[#5D6162]">Next</a>
+                        <a href="#" className="text-[20px] 1024:text-[7px] 1440:text-[10px] 2000:text-[22px] text-[#5D6162] font-fairplay tracking-wide inline-block pb-[2px] border-b border-[#5D6162]">Next</a>
                     </p>
                 </div>
             </>
