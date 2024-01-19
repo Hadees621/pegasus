d0efcae17fb011064ca6caa304154567d8245712
hadees621 <hadees621@gmail.com>
2024-01-19 13:18:14 +0500

    first box

--- src/pages/submissions/components/Section.js
+++ src/pages/submissions/components/Section.js
@@ -34,32 +34,49 @@ const Section = () => {
 
             <div className="flex flex-col 1024:flex-row w-full justify-center 1024:px-40 xl:px-52 2560:px-96 py-32 gap-10 border">
 
-                <div className="flex flex-col items-center justify-center text-center group bg-white border  w-full  ">
-                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset1.png" alt="Section Image" />
-                    <p className="text-center 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
-                        Completing your manuscript
+                <div className="flex flex-col items justify-center text-center group bg-white w-full mr-7 px-2 py-10">
+                    <p className="text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Publishing Process
                     </p>
-                </div>
+                    <p className="text-start text-[14px] 425:text-[12px] 1024:text-[7px] 1024:leading-4 1440:text-[9px] 1440:leading-6 2000:text-[15px] 2000:leading-8 2000:my-9 leading-6 font-roboto">
+                        Once your submission is received it is considered
+                        for publication by an editor, who will assess
+                        content, subject matter and suitability under one
+                        of our imprints
+                    </p>
 
+                    <button className="bg-[#031428] text-lg text-white rounded-3xl w-[150px] h-12 font-fairplay 1024:w-[60px] 1024:h-[18px] 1440:w-[80px] 1440:h-[24px] 1440:text-[10px] 2560:w-[130px] 2560:h-[45px] 2560:text-[17px] 1024:text-[8px] items-center flex justify-center font-extralight">
+                        Read more
+                    </button>
+
+                    <div className="flex gap-6 items-center py-8">
+                        <a href="#" className="flex items-center justify-center h-12 w-12 rounded-full bg-white border border-black p-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><g transform="rotate(-90 16 16)"><path fill="currentColor" d="m16 6.594l-.72.687l-12.5 12.5l1.44 1.44L16 9.437l11.78 11.78l1.44-1.437l-12.5-12.5z" /></g></svg>
+                        </a>
+                        <a href="#" className="flex items-center justify-center h-12 w-12 rounded-full bg-white border border-black p-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><g transform="rotate(90 16 16)"><path fill="currentColor" d="m16 6.594l-.72.687l-12.5 12.5l1.44 1.44L16 9.437l11.78 11.78l1.44-1.437l-12.5-12.5z" /></g></svg>
+                        </a>
+                        <p> 01 / 02</p>
+                    </div>
+                </div>
                 <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
                     <img className="flex items-center justify-center w-full" src="assests/submissions/Asset1.png" alt="Section Image" />
-                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                    <p className="text-center mt-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
                         Completing your manuscript
                     </p>
                 </div>
                 <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
                     <img className="flex items-center justify-center w-full" src="assests/submissions/Asset2.png" alt="Section Image" />
-                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
-                        Completing your manuscript
-                    </p>
-                </div><div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
-                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset3.png" alt="Section Image" />
-                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
-                        Completing your manuscript
+                    <p className="text-center mt-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Submission and editorial
                     </p>
                 </div>
-
-
+                <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
+                    <img className="flex items-center justify-center w-full pt-10" src="assests/submissions/Asset3.png" alt="Section Image" />
+                    <p className="text-center mt-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Offer of publication
+                    </p>
+                </div>
 
             </div>
 
