f691df75f65e0a51c249fd7af911fdc5cdfdf900
hadees621 <hadees621@gmail.com>
2024-01-19 16:47:30 +0500

    banner

--- src/pages/about/components/Section.js
+++ src/pages/about/components/Section.js
@@ -2,7 +2,7 @@ import React from "react";
 import { audioBooksData } from "../../homepage/utils/utils";
 import Believe from "./Believe";
 import Banner from "../../../components/Banner"
-
+import SubmitBanner from "../../../components/SubmitBanner";
 const Section = () => {
     return (
         <>
@@ -253,16 +253,13 @@ const Section = () => {
                     </div>
                 </div>
 
-                {/* Submit online */}
-                <div class="relative m-4 p-4 bg-cover bg-center 320:h-[380px] 1024:h-[200px] 1440:h-[270px] 2000:h-[450px] my-20" style={{ backgroundImage: `url('assests/section/section6.png')` }}>
-                    <div class="absolute 320:bottom-1 320:left-1 bottom-8 left-8 p-4 text-white 1024:p-1 1024:bottom-2 1024:left-4 1440:p-5 1440:bottom-2 2000:p-9 gap-0">
-                        <h3 class="320:text-[32px] text-[48px] font-fairplay text-white 1024:text-black 1024:text-[18px] 1440:text-[28px] 2000:text-[60px] -mb-2">Submit online
-                        </h3>
-                        <p class="320:text-[13px] text-base text-white 1024:text-black 1024:text-[8px] 1440:text-[10px] 2000:text-[16px]">Lorem ipsum dolor sit amet, consectetuer adipscing elit.
-                            .</p>
-                        <button className="bg-[#031428] text-lg text-white rounded-3xl 1440:w-[100px] 1440:h-7 1440:text-[11px] 2000:text-[15px] 2000:w-[150px] 2000:h-11 font-fairplay 1024:w-[60px] 1024:h-[18px] 1024:text-[8px] items-center flex justify-center font-extralight mt-2 1440:mt-3 2000:mt-7 320:w-[100px] 320:text-[13px]">Explore</button>
-                    </div>
-                </div>
+                <SubmitBanner
+                    title="Submit online"
+                    subtitle="Your subtitle here"
+                    description="Lorem ipsum dolor sit amet, consectetuer adipscing elit."
+                    buttonText="Submit online"
+                    imageUrl="assests/section/section6.png"
+                />
 
             </div>
         </>
