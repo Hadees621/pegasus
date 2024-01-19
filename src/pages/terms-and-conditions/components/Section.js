7ef84a540eb6f9cb66a0ce454c03811986a88142
hadees621 <hadees621@gmail.com>
2024-01-19 18:22:02 +0500

    asaas

--- src/pages/terms-and-conditions/components/Section.js
+++ src/pages/terms-and-conditions/components/Section.js
@@ -3,7 +3,59 @@ import React from "react";
 const Section = () => {
     return (
         <>
-            Section
+            <div className="w-full justify-center items-center 320:px-3 425:px-4 768:px-10 1024:px-[240px] 1440:px-[400px] 1535:px-[430px] 2000:px-[500px] 2560:px-[700px] py-28 1024:py-10 1440:py-24">
+                <div className="flex flex-col 1024:flex-row">
+                    <div className="w-full 1024:w-[27%] space-y-6 1024:space-y-3 2000:space-y-6 1024:pr-10">
+                        <h1
+                            className={`text-center 1024:text-start text-[20px] 1024:text-[12px] 1440:text-[15px] 2000:text-[24px]  font-fairplay cursor-pointer`}
+                        >
+                            asd;klsfnsdkfnskl;n
+                        </h1>
+                        <h1
+                            className={`text-center 1024:text-start text-[20px] 1024:text-[12px] 1440:text-[15px] 2000:text-[24px]  font-fairplay cursor-pointer`}
+                        >
+                            asd;klsfnsdkfnskl;n
+                        </h1>
+                        <h1
+                            className={`text-center 1024:text-start text-[20px] 1024:text-[12px] 1440:text-[15px] 2000:text-[24px]  font-fairplay cursor-pointer`}
+                        >
+                            asd;klsfnsdkfnskl;n
+                        </h1> <h1
+                            className={`text-center 1024:text-start text-[20px] 1024:text-[12px] 1440:text-[15px] 2000:text-[24px]  font-fairplay cursor-pointer`}
+                        >
+                            asd;klsfnsdkfnskl;n
+                        </h1>
+
+                    </div>
+
+                    <div className="w-full 1024:w-[73%] mt-10 1024:mt-0">
+                        {/* Dummy Data */}
+                        <h1 className="text-center 1024:text-start text-[24px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                            Dummy Heading
+                        </h1>
+                        <p className="text-center 1024:text-start text-[14px] 1024:text-[8px] 1024:leading-4 1440:text-[12px] 1440:leading-6 2000:text-[18px] 2000:leading-8 1024:mt-2 mt-6 leading-6 font-roboto">
+
+                            Dummy Text. Replace this with your actual content.
+                            Dummy Text. Replace this with your actual content.
+                            Dummy Text. Replace this with your actual content.
+                            Dummy Text. Replace this with your actual content.
+                            Dummy Text. Replace this with your actual content.
+                        </p>
+
+                        <p className="text-[14px] 2000:text-[18px] text-center 1024:text-start">
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+                            More dummy text. Replace this with your actual content.
+
+                        </p>
+                    </div>
+                </div>
+
+            </div>
         </>
     );
 };
