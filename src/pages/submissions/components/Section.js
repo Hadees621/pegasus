9c7a5d2ecb8ba83a566eaa92eeee5a86472b3da7
hadees621 <hadees621@gmail.com>
2024-01-19 12:47:15 +0500

    boxes section

--- src/pages/submissions/components/Section.js
+++ src/pages/submissions/components/Section.js
@@ -6,7 +6,7 @@ import FaqDemo from "./FaqDemo"
 const Section = () => {
     return (
         <>
-            <div className="bg-[#F1F1F3]  justify-center 320:m-5 1024:h-48 1440:h-60 1535:h-[270px] 2000:h-[370px] 1440:flex-row 1024:mx-40 xl:mx-52 1535:mx-[350px] border border-[#F1F1F3]">
+            <div className="bg-[#F1F1F3] justify-center 320:m-5 1024:h-48 1440:h-60 1535:h-[270px] 2000:h-[370px] 1440:flex-row 1024:mx-40 xl:mx-52 1535:mx-[350px] border border-[#F1F1F3]">
                 <p className="320:text-[12px] 320:m-5 2000:text-sm font-roboto m-10 2000:m-10 1024:text-[8px] 1024:m-4 1440:text-[9px] 1440:m-5">
                     Home — Submissions
                 </p>
@@ -19,8 +19,7 @@ const Section = () => {
 
             <TextSection />
 
-            {/* History */}
-
+            {/* FAQ */}
             <div className=" bg-[#F8F5EF] py-28 1024:py-10 1440:py-12">
                 <div className="flex flex-col 1024:flex-row w-full justify-center 768:px-12 1024:px-56 1440:px-[400px] 1535:px-[430px] 2000:px-[580px] 2560:px-[700px] gap-8 1024:gap-0 1440:gap-4 2000:gap-8">
                     <div className="w-full 1024:w-[20%]">
@@ -33,7 +32,47 @@ const Section = () => {
                 </div>
             </div>
 
+            <div className="flex flex-col 1024:flex-row w-full justify-center 1024:px-40 xl:px-52 2560:px-96 py-32 gap-10 border">
 
+                <div className="flex flex-col items-center justify-center text-center group bg-white border  w-full  ">
+                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset1.png" alt="Section Image" />
+                    <p className="text-center 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Completing your manuscript
+                    </p>
+                </div>
+
+                <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
+                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset1.png" alt="Section Image" />
+                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Completing your manuscript
+                    </p>
+                </div>
+                <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
+                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset2.png" alt="Section Image" />
+                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Completing your manuscript
+                    </p>
+                </div><div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
+                    <img className="flex items-center justify-center w-full" src="assests/submissions/Asset3.png" alt="Section Image" />
+                    <p className="text-center my-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
+                        Completing your manuscript
+                    </p>
+                </div>
+
+
+
+            </div>
+
+            {/* Submit online */}
+            {/* <div class="relative m-4 p-4 bg-cover bg-center 320:h-[380px] 2560:h-[580px] 1024:h-[180px] 1440:h-[330px] mt-10" style={{ backgroundImage: `url('assests/section/section6.png')` }}>
+                <div class="absolute 320:bottom-1 320:left-1 bottom-8 left-8 p-4 text-white 1024:p-1 1024:bottom-2 1024:left-4 1440:p-5 1440:bottom-2 2560:p-9 gap-0">
+                    <h3 class="320:text-[32px] text-[48px] font-fairplay text-white 1024:text-black 1024:text-[18px] 1440:text-[28px] 2560:text-[60px] -mb-2">Submit online
+                    </h3>
+                    <p class="320:text-[13px] text-base text-white 1024:text-black 1024:text-[8px] 1440:text-[10px] 2560:text-[16px]">Lorem ipsum dolor sit amet, consectetuer adipscing elit.
+                        .</p>
+                    <button className="bg-[#031428] text-lg text-white rounded-3xl 1440:w-[100px] 1440:h-7 1440:text-[11px] 2560:text-[15px] 2560:w-[150px] 2560:h-11 font-fairplay 1024:w-[60px] 1024:h-[18px] 1024:text-[8px] items-center flex justify-center font-extralight mt-2 1440:mt-3 2560:mt-7 320:w-[100px] 320:text-[13px]">Explore</button>
+                </div>
+            </div> */}
 
         </>
     );
