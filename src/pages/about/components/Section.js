65bc048f9d33ad199274eaa5a4b42eef5fd8a34b
hadees621 <hadees621@gmail.com>
2024-01-19 16:29:59 +0500

    refractoring

--- src/pages/about/components/Section.js
+++ src/pages/about/components/Section.js
@@ -12,17 +12,6 @@ const Section = () => {
                         great publisher requires, we have been successfully publishing high
                         quality books for over 20 years." />
 
-            {/* <div className="bg-[#F1F1F3]  justify-center 320:m-5 1024:h-48 1440:h-60 1535:h-[270px] 2000:h-[370px] 1440:flex-row 1024:mx-40 xl:mx-52 1535:mx-[350px] border border-[#F1F1F3]">
-                <p className="320:text-[12px] 320:m-5 2000:text-sm font-roboto m-10 2000:m-10 1024:text-[8px] 1024:m-4 1440:text-[9px] 1440:m-5">
-                   
-                </p>
-                <div className="flex w-full justify-center">
-                    <p className="320:text-4xl 768:text-5xl 1024:text-[25px] 1440:text-[45px] 2000:text-[70px] flex justify-center font-fairplay my-24 border-b-2 border-black shadow-sm w-max 320:mt-16 320:mb-6 1024:mt-12 1024:mb-0 1440:mb-1 2000:mb-3 pb-3">
-                       
-                    </p>
-                </div>
-                
-            </div> */}
 
             {/* padded section 1 */}
             <div className="w-full justify-center 320:px-6 768:px-12 1024:px-52 1440:px-[290px] 1535:px-[360px] 2000:px-[360px] ">
--- src/pages/features/Features.js
+++ src/pages/features/Features.js
@@ -2,10 +2,9 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/header/Header";
 import Section from "./components/Section";
-import { articleData } from "./utils";
 
 const Features = () => {
-   
+
     return (
         <>
             <Header />
