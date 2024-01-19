c69944c9496fcd2961a3c3fe28a0cd2f3e768cf9
hadees621 <hadees621@gmail.com>
2024-01-19 13:27:02 +0500

    picture section added

--- src/pages/submissions/components/Section.js
+++ src/pages/submissions/components/Section.js
@@ -32,7 +32,7 @@ const Section = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col 1024:flex-row w-full justify-center 1024:px-40 xl:px-52 2560:px-96 py-32 gap-10 border">
+            <div className="flex flex-col 1024:flex-row w-full justify-center 1024:px-40 xl:px-52 2560:px-96 py-32 gap-10">
 
                 <div className="flex flex-col items justify-center text-center group bg-white w-full mr-7 px-2 py-10">
                     <p className="text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
@@ -59,6 +59,7 @@ const Section = () => {
                         <p> 01 / 02</p>
                     </div>
                 </div>
+
                 <div className="flex flex-col items-center justify-center text-center group bg-white w-full border shadow-xl p-10">
                     <img className="flex items-center justify-center w-full" src="assests/submissions/Asset1.png" alt="Section Image" />
                     <p className="text-center mt-8 425:text-start text-[24px] 425:text-[16px] 1024:text-[14px] 1440:text-[16px] 2000:text-[28px] text-[#B79248] font-fairplay">
@@ -81,15 +82,19 @@ const Section = () => {
             </div>
 
             {/* Submit online */}
-            {/* <div class="relative m-4 p-4 bg-cover bg-center 320:h-[380px] 2560:h-[580px] 1024:h-[180px] 1440:h-[330px] mt-10" style={{ backgroundImage: `url('assests/section/section6.png')` }}>
-                <div class="absolute 320:bottom-1 320:left-1 bottom-8 left-8 p-4 text-white 1024:p-1 1024:bottom-2 1024:left-4 1440:p-5 1440:bottom-2 2560:p-9 gap-0">
-                    <h3 class="320:text-[32px] text-[48px] font-fairplay text-white 1024:text-black 1024:text-[18px] 1440:text-[28px] 2560:text-[60px] -mb-2">Submit online
-                    </h3>
-                    <p class="320:text-[13px] text-base text-white 1024:text-black 1024:text-[8px] 1440:text-[10px] 2560:text-[16px]">Lorem ipsum dolor sit amet, consectetuer adipscing elit.
-                        .</p>
-                    <button className="bg-[#031428] text-lg text-white rounded-3xl 1440:w-[100px] 1440:h-7 1440:text-[11px] 2560:text-[15px] 2560:w-[150px] 2560:h-11 font-fairplay 1024:w-[60px] 1024:h-[18px] 1024:text-[8px] items-center flex justify-center font-extralight mt-2 1440:mt-3 2560:mt-7 320:w-[100px] 320:text-[13px]">Explore</button>
+            <div className="1024:px-40 xl:px-52 2560:px-96 pb-20">
+                <div class="relative m-4 p-4 bg-cover bg-center 320:h-[380px] 2560:h-[580px] 1024:h-[180px] 1440:h-[330px] mt-10" style={{ backgroundImage: `url('assests/section/section6.png')` }}>
+                    <div class="absolute 320:bottom-1 320:left-1 bottom-8 left-8 p-4 text-white 1024:p-1 1024:bottom-2 1024:left-4 1440:p-5 1440:bottom-2 2560:p-9 gap-0">
+                        <h3 class="320:text-[32px] text-[48px] font-fairplay text-white 1024:text-black 1024:text-[18px] 1440:text-[28px] 2560:text-[60px] -mb-2">Submit online
+                        </h3>
+                        <p class="320:text-[13px] text-base text-white 1024:text-black 1024:text-[8px] 1440:text-[10px] 2560:text-[16px]">Lorem ipsum dolor sit amet, consectetuer adipscing elit.
+                            .</p>
+                        <button className="bg-[#031428] text-lg text-white rounded-3xl 1440:w-[100px] 1440:h-7 1440:text-[11px] 2560:text-[15px] 2560:w-[150px] 2560:h-11 font-fairplay 1024:w-[60px] 1024:h-[18px] 1024:text-[8px] items-center flex justify-center font-extralight mt-2 1440:mt-3 2560:mt-7 320:w-[100px] 320:text-[13px]">
+                            Submit online
+                        </button>
+                    </div>
                 </div>
-            </div> */}
+            </div>
 
         </>
     );
