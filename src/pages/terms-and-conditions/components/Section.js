62438a9b3ac49ea37247d17c5639b471e9b2b3b8
hadees621 <hadees621@gmail.com>
2024-01-22 12:40:09 +0500

    done for terms

--- src/pages/terms-and-conditions/components/Section.js
+++ src/pages/terms-and-conditions/components/Section.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
-import Terms from "./Terms"
+import Terms from "./Terms";
 import PoliciesContent from "./PoliciesContent";
 import ShippingContent from "./ShippingContent";
 import RightsContent from "./RightsContent";
 
 const Section = () => {
     const [selectedHeading, setSelectedHeading] = useState("Terms & Conditions");
-    const [activeMenuItem, setActiveMenuItem] = useState("Terms & Conditions");
 
     const handleHeadingClick = (heading) => {
         setSelectedHeading(heading);
-        setActiveMenuItem(heading);
     };
 
     const renderContentComponent = () => {
@@ -31,28 +29,32 @@ const Section = () => {
     return (
         <>
             <div className="w-full justify-center items-center 320:px-3 425:px-4 768:px-10 1024:px-[240px] 1440:px-[400px] 1535:px-[430px] 2000:px-[500px] 2560:px-[700px] py-28 1024:py-10 1440:py-24">
-                <div className="flex flex-col 1024:flex-row">
+                <div className="flex flex-col 1024:flex-row space-y-14 1024:space-y-0">
                     <div className="w-full 1024:w-[27%] space-y-6 1024:space-y-3 2000:space-y-7 1024:pr-10">
                         <h1
-                            className={`text-center 1024:text-start text-[24px] 1024:text-[14px] 1440:text-[16px] 2000:text-[20px] ${activeMenuItem ? "text-[#B79248]" : "text-black"} font-fairplay cursor-pointer`}
+                            className={`text-center 1024:text-start text-[24px] 1024:text-[12px] 1440:text-[16px] 2000:text-[20px] ${selectedHeading === "Terms & Conditions" ? "text-[#B79248]" : "text-black"
+                                } font-fairplay cursor-pointer`}
                             onClick={() => handleHeadingClick("Terms & Conditions")}
                         >
                             Terms & Conditions
                         </h1>
                         <h1
-                            className={`text-center 1024:text-start text-[24px] 1024:text-[14px] 1440:text-[16px] 2000:text-[20px] ${activeMenuItem ? "text-[#B79248]" : "text-black"} font-fairplay cursor-pointer`}
+                            className={`text-center 1024:text-start text-[24px] 1024:text-[12px] 1440:text-[16px] 2000:text-[20px] ${selectedHeading === "Policies & Cookies" ? "text-[#B79248]" : "text-black"
+                                } font-fairplay cursor-pointer`}
                             onClick={() => handleHeadingClick("Policies & Cookies")}
                         >
                             Policies & Cookies
                         </h1>
                         <h1
-                            className={`text-center 1024:text-start text-[24px] 1024:text-[14px] 1440:text-[16px] 2000:text-[20px] ${activeMenuItem ? "text-[#B79248]" : "text-black"} font-fairplay cursor-pointer`}
+                            className={`text-center 1024:text-start text-[24px] 1024:text-[12px] 1440:text-[16px] 2000:text-[20px] ${selectedHeading === "Shipping & Returns" ? "text-[#B79248]" : "text-black"
+                                } font-fairplay cursor-pointer`}
                             onClick={() => handleHeadingClick("Shipping & Returns")}
                         >
                             Shipping & Returns
                         </h1>
                         <h1
-                            className={`text-center 1024:text-start text-[24px] 1024:text-[14px] 1440:text-[16px] 2000:text-[20px] text-black ${activeMenuItem && "text-[#B79248]"} font-fairplay cursor-pointer`}
+                            className={`text-center 1024:text-start text-[24px] 1024:text-[12px] 1440:text-[16px] 2000:text-[20px] ${selectedHeading === "Rights & Permissions" ? "text-[#B79248]" : "text-black"
+                                } font-fairplay cursor-pointer`}
                             onClick={() => handleHeadingClick("Rights & Permissions")}
                         >
                             Rights & Permissions
