b177ebaba1b3ce2e802fd8a2808577dac278ce01
hadees621 <hadees621@gmail.com>
2024-02-01 12:33:56 +0500

    all done

--- src/pages/publishing-process/components/Middle.js
+++ src/pages/publishing-process/components/Middle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Middle = ({ data }) => {
     return data.map((item, index) => (
-        <div key={index} className={`flex flex-col items-center 1024:flex-row md:px-10 1024:px-40 xl:px-52 2560:px-96 1024:py-6 2000:py-32 ${index % 2 === 1 ? '1024:flex-row-reverse' : ''}`}>
+        <div key={index} className={`flex flex-col items-center 1024:flex-row md:px-10 1024:px-40 xl:px-52 2560:px-96 1024:py-6 2000:py-12 ${index % 2 === 1 ? '1024:flex-row-reverse' : ''}`}>
 
             <div className={`w-full 1024:w-1/2 bg-[#F9FAFA] hidden 1024:block ${item.imageClass} flex items-center justify-center`}>
                 <img
@@ -14,22 +14,22 @@ const Middle = ({ data }) => {
 
             <div className={`w-full 1024:w-1/2 flex flex-col justify-center p-8 1024:p-3 ${index % 2 === 0 ? '1024:pr-24 2000:pr-72 2000:pl-8' : '1024:pl-24 2000:pl-72 2000:pr-8'} space-y-4 1024:space-y-2 2000:space-y-7`}>
                 <div>
-                    <h1 className="font-fairplay text-[20px] 1024:text-[12px] 2000:text-[30px] text-[#90949D]">{item.title}</h1>
-                    <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:text-[30px] 2000:leading-9">{item.description}</p>
+                    <h1 className="font-fairplay text-[20px] 1024:text-[12px] 1440:text-[14px] 2000:text-[30px] text-[#90949D]">{item.title}</h1>
+                    <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.description}</p>
                 </div>
                 {item.optionalParagraph && (
                     <div>
-                        <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:leading-9">{item.optionalParagraph}</p>
+                        <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.optionalParagraph}</p>
                     </div>
                 )}
                 {item.optionalParagraph2 && (
                     <div>
-                        <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:leading-9">{item.optionalParagraph2}</p>
+                        <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.optionalParagraph2}</p>
                     </div>
                 )}
                 <div>
-                    <h3 className="font-bold font-fairplay leading-loose 1024:text-[9px]">Tip tip</h3>
-                    <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:leading-9">{item.tipDescription}</p>
+                    <h3 className="font-bold font-fairplay leading-loose 1024:text-[9px] 1440:text-[11px]">Tip tip</h3>
+                    <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.tipDescription}</p>
                 </div>
             </div>
 
@@ -38,39 +38,3 @@ const Middle = ({ data }) => {
 };
 
 export default Middle;
-
-// import React from 'react';
-
-// const Middle = ({ data }) => {
-//     return data.map((item, index) => (
-//         <div key={index} className={`flex flex-col 1024:flex-row md:px-10 1024:px-40 xl:px-52 2560:px-96 1024:py-6 2000:py-32 ${index % 2 === 1 ? '1024:flex-row-reverse' : ''}`}>
-
-//             <div className={`w-full 1024:w-1/2 bg-[#F9FAFA] hidden 1024:block ${item.imageClass} `}>
-//                 <img
-//                     src={item.imageSrc}
-//                     alt={item.altText}
-//                     className="w-full "
-//                 />
-//             </div>
-
-//             <div className={`w-full 1024:w-1/2 flex flex-col justify-center p-8 1024:p-3 ${index % 2 === 0 ? '1024:pr-24 2000:pr-72 2000:pl-8' : '1024:pl-24 2000:pl-72 2000:pr-8'} space-y-4 1024:space-y-2 2000:space-y-7`}>
-//                 <div>
-//                     <h1 className="font-fairplay text-[20px] 1024:text-[12px] 2000:text-[30px] text-[#90949D]">{item.title}</h1>
-//                     <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:text-[30px] 2000:leading-9">{item.description}</p>
-//                 </div>
-//                 {item.optionalParagraph && (
-//                     <div>
-//                         <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:leading-9">{item.optionalParagraph}</p>
-//                     </div>
-//                 )}
-//                 <div>
-//                     <h3 className="font-bold font-fairplay leading-loose 1024:text-[9px]">Tip tip</h3>
-//                     <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 2000:leading-9">{item.tipDescription}</p>
-//                 </div>
-//             </div>
-
-//         </div>
-//     ));
-// };
-
-// export default Middle;
--- src/pages/publishing-process/utils.js
+++ src/pages/publishing-process/utils.js
@@ -6,7 +6,7 @@ export const componentsData = [
         description: "You’ve done the hard part... You’ve written a book. Your book. We understand that a manuscript represents much more than just a collection of words and tales. What is laying on your desk is the product of hundreds of head scratches and endless cups of coffee (or tea) and those noble grins when you write those lines you’re so proud of.",
         tipDescription: "Get your friends and family to review your work once you have finished writing and improve it as necessary before sending your work to us.",
         optionalParagraph: "",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 2000:py-20 2000:pl-80 2000:pr-32"
+        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-10 1440:pl-44 1440:pr-20 2000:py-20 2000:pl-80 2000:pr-32"
     },
     {
         imageSrc: "assests/publishing/Asset2.png",
@@ -15,7 +15,7 @@ export const componentsData = [
         description: "The Submissions Editor reviews your submission to ensure that your manuscript is complete, and that we have all the information we need. The manuscript is then handed over to the editorial team for editorial review",
         optionalParagraph: "Our editors are looking for a page-tuner; something original and unique. A work with potential to appeal to a wide array of readers.Editorial review also takes styling, spelling and grammatical consistency into account, so make sure you proof your work before sending it to us.This review process can take up to 6 weeks",
         tipDescription: "Be patient during the review process, we always get back to all submissions and let you know our decision.",
-        imageClass: "1024:py-6 1024:pr-36 1024:pl-12 2000:py-20 2000:pr-80 2000:pl-32"
+        imageClass: "1024:py-6 1024:pr-36 1024:pl-12 1440:py-10 1440:pr-44 1440:pl-20 2000:py-20 2000:pr-80 2000:pl-32"
     },
     {
         imageSrc: "assests/publishing/Asset3.png",
@@ -24,7 +24,7 @@ export const componentsData = [
         description: "If our editors like what they read, you will be contacted with an offer of publication. On signing the publishing contract you’ve become part of the Olympia family – Congratulations!",
         optionalParagraph: "This is where it all begins. Your manuscript is now handed over to your dedicated production coordinator. You’ll be working with them to finalise ideas for your blurb, acknowledgments, illustrations if necessary as well as other key points. This is where we start to bring your book to life",
         tipDescription: "In the next stage of producing the book, patience and corporation is key as it can take few months to get your book ready.",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 2000:py-24 2000:pl-80 2000:pr-32"
+        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-12 1440:pl-44 1440:pr-20 2000:py-24 2000:pl-80 2000:pr-32"
     },
     {
         imageSrc: "assests/publishing/Asset4.png",
@@ -33,7 +33,7 @@ export const componentsData = [
         description: "You shouldn’t judge a book by it’s cover. But let’s be honest, we do. This is why we work so closely with our graphics designers on creating dazzling covers. Yourself and the production team work on a cover idea, when the idea is finalised, the graphics designer will work to create sample covers which will be sent for your approval.",
         optionalParagraph: "Once both parties are happy, the cover is finalised.",
         tipDescription: "Simple covers are often more attractive.",
-        imageClass: "1024:py-6 1024:pr-36 1024:pl-12 2000:py-20 2000:pr-80 2000:pl-32"
+        imageClass: "1024:py-6 1024:pr-36 1024:pl-12 1440:py-10 1440:pr-44 1440:pl-20 2000:py-20 2000:pr-80 2000:pl-32"
     },
     {
         imageSrc: "assests/publishing/Asset5.png",
@@ -42,7 +42,7 @@ export const componentsData = [
         description: "Pages are being transformed into a book. Next, your raw manuscript will be formatted transforming it into a book layout. Text type for the book will be selected based on various factors such as the genre of the book. Details such as book size will also be confirmed to prepare your book for the proofing stage.",
         optionalParagraph: "",
         tipDescription: "Whilst your book is in production, use this time to perhaps work on your next book. Producing your book can take up to a year",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 2000:py-20 2000:pl-80 2000:pr-32"
+        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-12 1440:pl-44 1440:pr-20 2000:py-20 2000:pl-80 2000:pr-32"
     },
     {
         imageSrc: "assests/publishing/Asset6.png",
@@ -51,7 +51,7 @@ export const componentsData = [
         description: "Did you know that spelling mistakes are one of the most irritating errors for a reader? So, we’ll be taking extra care of that almost straight away with intense proofing. Patience is virtue during this stage of producing the book. Good things take time and care. Your manuscript is proofed and corrections, edits and changes are suggested. The first version is sent to you for approval. This process is repeated until we are both happy with the changes put forward. Following this, your book is prepared for printing and you’ll be contacted by your dedicated publicist. Soon it is time to let the world know about your book.",
         optionalParagraph: "",
         tipDescription: "Perfect time to get a few portraits clicked of yourself. These will be used to promote you and your book.",
-        imageClass: "1024:py-12 1024:px-20  2000:py-28 2000:px-44"
+        imageClass: "1024:py-12 1024:px-20 1440:py-14 1440:px-28 2000:py-28 2000:px-44"
     },
     {
         imageSrc: "assests/publishing/Asset7.png",
@@ -60,7 +60,7 @@ export const componentsData = [
         description: "Your publicist will get in touch with you with a provisional publication date and will look beneath the story to find out what makes your book special and most importantly create a promotional plan before entering the ever-changing, ever-expanding book market",
         optionalParagraph: "Press releases are created, marketing material is finalised, distributors such as Gardners and Nielsens are sent your book available to order. Your publicist will also work their magic in contacting specialist, independent and well-known bookshops and media outlets where your book can be promoted.",
         tipDescription: "Clear your diary it is time for you to host some events!",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 2000:py-20 2000:pl-80 2000:pr-32"
+        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-12 1440:pl-44 1440:pr-20 2000:py-20 2000:pl-80 2000:pr-32"
     },
     {
         imageSrc: "assests/publishing/Asset8.png",
@@ -70,7 +70,7 @@ export const componentsData = [
         optionalParagraph: "In our decade in publishing industry we have developed many rich relationships with media outlets, independent bookshops, printers, distributors, book reviewers, radio stations and journalist which sets us aside from others. However, your publicists’ most valued asset of promoting your book is yourself as the author The more you are actively involved the better the success of your book.",
         optionalParagraph2: "The more you are actively involved the better the success of your book.",
         tipDescription: "You can use your own social media channels to engage and connect with your readers and fans. You can also start a blog to promote your book!",
-        imageClass: "1024:py-6 1024:pr-36 1024:pl-14 2000:py-20 2000:pr-72 2000:pl-32"
+        imageClass: "1024:py-6 1024:pr-36 1024:pl-14 1440:py-10 1440:pr-44 1440:pl-20 2000:py-20 2000:pr-72 2000:pl-32"
     },
     {
         imageSrc: "assests/publishing/Asset9.png",
@@ -79,7 +79,7 @@ export const componentsData = [
         description: "By this time your book will have arrived in our warehouse and we delve into the digital elements of promotions. An eBook is also designed and created, banners and promotional videos are brought to life and the social media campaigns are kick-started! The complimentary copies will also be sent to you. This is also the time an audio book will be considered at no cost to the author, book awards and prizes will be researched and we will discuss thinking of ideas out the box to get your book out there!",
         optionalParagraph: "Get ready to capture one of the most memorable time of your life - as many authors will confirm, there’s nothing that compares to the feeling of holding your book for the first time",
         tipDescription: "The more you are actively involved in promotions the better the success of your book.",
-        imageClass: "1024:py-12 1024:pl-32 1024:pr-20 000:py-36 2000:pl-80 2000:pr-28"
+        imageClass: "1024:py-12 1024:pl-32 1024:pr-20 1440:py-12 1440:pl-44 1440:pr-20 2000:py-28 2000:pl-80 2000:pr-28"
     },
     {
         imageSrc: "assests/publishing/Asset10.png",
@@ -88,6 +88,6 @@ export const componentsData = [
         description: "u can look forward to royalties update every 6 months. Reviews from your readers is always a joy and reward of its own. Rising opportunities that come hand-in-hand with being a published author with an independent publishing house behind them.",
         optionalParagraph: "What started as an idea now has a life of it’s own– and this, is only the beginning!",
         tipDescription: "Enjoy and treasure the moment of holding your book for the first time. Often this is our authors most memorable moment of their publishing journey.",
-        imageClass: "1024:py-10 1024:pr-36 1024:pl-14 2000:py-20 2000:pr-80 2000:pl-32"
+        imageClass: "1024:py-10 1024:pr-36 1024:pl-14 1440:py-10 1440:pr-44 1440:pl-20 2000:py-24 2000:pr-80 2000:pl-32"
     },
 ];
