1eff0a388f9e2c4eb64526e9a8395c57f8a05f0c
hadees621 <hadees621@gmail.com>
2024-02-12 11:47:18 +0500

    implemented

--- src/pages/sustainability/components/Middle.js
+++ src/pages/sustainability/components/Middle.js
@@ -1,31 +1,33 @@
 import React from 'react';
+import { susComponentData } from "../utils"
 
-const Middle = ({ data }) => {
-    return data.map((item, index) => (
-        <div key={index} className={`flex flex-col items-center 1024:flex-row md:px-10 1024:px-40 xl:px-52 2560:px-96 1024:py-6 2000:py-12 ${index % 2 === 1 ? '1024:flex-row-reverse' : ''}`}>
+const Middle = ({ }) => {
 
-            <div className={`w-full 1024:w-1/2 bg-[#F9FAFA] 1024:block flex items-center justify-center`}>
+    return susComponentData.map((item, index) => (
+        <div key={index} className={`flex flex-col items-center 1024:flex-row md:px-10 1024:px-40 xl:px-52 2560:px-96 1024:py-6 2000:py-12 ${index % 2 === 0 ? '1024:flex-row-reverse' : ''}`}>
+
+            <div className={`w-full 1024:w-1/2 bg-[#F9FAFA] 1024:block flex items-center justify-center ${item.imageClass}`}>
                 <img
                     src={item.imageSrc}
-                    className="max-w-full h-auto"
+                    className="max-w-full h-[200px] 1024:h-[110px] 1440:h-[140px] 2000:h-[200px]"
                 />
             </div>
 
-            <div className={`w-full 1024:w-1/2 flex flex-col justify-center p-8 1024:p-3 ${index % 2 === 0 ? '1024:pr-24 2000:pr-72 2000:pl-8' : '1024:pl-24 2000:pl-72 2000:pr-8'} space-y-4 1024:space-y-2 2000:space-y-7`}>
+            <div className={`w-full 1024:w-1/2 flex flex-col justify-center p-8 1024:p-3 ${index % 2 === 0 ? '1024:pl-24 2000:pl-72 2000:pr-8' : '1024:pr-24 2000:pr-72 2000:pl-8'} space-y-4 1024:space-y-2 2000:space-y-7`}>
                 <div>
                     <h1 className="font-fairplay text-[20px] 1024:text-[12px] 1440:text-[14px] 2000:text-[30px] text-[#90949D]">{item.title}</h1>
                 </div>
-                {item.optionalParagraph && (
+                {item.para1 && (
                     <div>
                         <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.para1}</p>
                     </div>
                 )}
-                {item.optionalParagraph2 && (
+                {item.para2 && (
                     <div>
                         <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.para2}</p>
                     </div>
                 )}
-                {item.optionalParagraph3 && (
+                {item.para3 && (
                     <div>
                         <p className="text-[14px] leading-7 1024:text-[8px] 1024:leading-4 1440:text-[10px] 1440:leading-5 2000:text-[14px] 2000:leading-9">{item.para3}</p>
                     </div>
@@ -33,7 +35,8 @@ const Middle = ({ data }) => {
             </div>
 
         </div>
-    ));
+    ))
+    // ));
 };
 
 export default Middle;
--- src/pages/sustainability/utils.js
+++ src/pages/sustainability/utils.js
@@ -1,30 +1,31 @@
-const susComponentData = [
+export const susComponentData = [
     {
-        imageSrc: "assests/publishing/Asset1.png",
+        imageSrc: "assests/sus/Asset1.png",
         altText: "Imprint 1",
         title: "Paper sourcing",
         para1: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        para2: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        para2: "Lorem ipsum dolor sit amet,  consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation",
         para3: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-10 1440:pl-44 1440:pr-20 2000:py-20 2000:pl-80 2000:pr-32"
+        imageClass: "py-10 1024:py-8 1024:pr-40 1024:pl-16 1440:py-12 1440:pr-64 1440:pl-20 2000:py-32 2000:pr-80 2000:pl-32"
     },
     {
-        imageSrc: "assests/publishing/Asset2.png",
-        altText: "Imprint 1",
+        imageSrc: "assests/sus/Asset3.png",
+        altText: "In-office practises",
         title: "2 — Submission and editorial review",
-        para1: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        para2: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        para3: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        imageClass: "1024:py-6 1024:pr-36 1024:pl-12 1440:py-10 1440:pr-44 1440:pl-20 2000:py-20 2000:pr-80 2000:pl-32"
+        para1: " - Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        para2: " - Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        para3: " - Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        imageClass: "py-10 1024:py-10 1024:pl-44 1024:pr-16 1440:py-14 1440:pl-60 1440:pr-16 2000:py-32 2000:pl-[500px] 2000:pr-10"
     },
     {
-        imageSrc: "assests/publishing/Asset3.png",
+        imageSrc: "assests/sus/Asset2.png",
         altText: "Imprint 3",
-        title: "3 — Offer of publication",
+        title: "How we are helping our environment",
         para1: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        para2: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        para3: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
-        imageClass: "1024:py-8 1024:pl-32 1024:pr-12 1440:py-12 1440:pl-44 1440:pr-20 2000:py-24 2000:pl-80 2000:pr-32"
-    }
+        para2: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+        para3: "",
+        imageClass: "py-10 1024:py-10 1024:pr-44 1024:pl-16 1440:py-6 1440:pr-64 1440:pl-20 2000:py-32 2000:pr-60 2000:pl-32"
+    },
+
 ];
 
