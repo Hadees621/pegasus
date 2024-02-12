05e33466cefe799e7820eaff9684a7b70d8f4cb5
hadees621 <hadees621@gmail.com>
2024-02-12 12:26:20 +0500

    till 1024

--- src/pages/wishlist/components/DropdownMenu.js
+++ src/pages/wishlist/components/DropdownMenu.js
@@ -19,13 +19,13 @@ function DropdownMenu() {
         <div className="relative z-10">
             <button
                 onClick={toggleDropdown}
-                className="flex items-center justify-between py-2 border-[#D4D4D8] focus:outline-none w-full border-y"
+                className="flex items-center justify-between py-2 1024:py-1 border-[#D4D4D8] focus:outline-none w-full border-y"
             >
                 <div className='w-full flex justify-between'>
-                    <p className={`text-[12px] ${selectedFormat === "Select Format" ? 'text-[#B79248]' : 'text-black'} font-roboto`}>{selectedFormat}</p>
-                    {selectedFormat !== "Select Format" && <p className="text-black text-[12px]">{selectedPrice}</p>}
+                    <p className={`text-[12px] 1024:text-[10px] ${selectedFormat === "Select Format" ? 'text-[#B79248]' : 'text-black'} font-roboto`}>{selectedFormat}</p>
+                    {selectedFormat !== "Select Format" && <p className="text-black text-[12px] 1024:text-[9px]">{selectedPrice}</p>}
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className={`w-10 h-7 transition-transform transform ${isOpen ? '' : 'rotate-180'}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className={`w-10 h-7 1024:w-6 transition-transform transform ${isOpen ? '' : 'rotate-180'}`}>
                     <path fill="#8D8F8F" d="m16.854 13.647l-4.5-4.5a.5.5 0 0 0-.707 0l-4.5 4.5a.5.5 0 0 0 .707.707L12 10.207l4.146 4.147a.498.498 0 0 0 .707 0a.5.5 0 0 0 0-.707" />
                 </svg>
             </button>
@@ -33,21 +33,21 @@ function DropdownMenu() {
                 isOpen && (
                     <div className="absolute mt-2 w-full bg-white rounded-md z-20">
                         <ul className="space-y-0 2000:space-y-2">
-                            <li onClick={() => handleItemClick("Paper back", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay">
+                            <li onClick={() => handleItemClick("Paper back", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay text-[13px] 1024:text-[10px]">
                                 <p className={selectedFormat === "Paper back" ? 'text-[#B79248]' : 'text-black'}>Paper back</p>
-                                <p className="text-black text-[13px]">£0.00</p>
+                                <p className="text-black ">£0.00</p>
                             </li>
-                            <li onClick={() => handleItemClick("Hard back", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay">
+                            <li onClick={() => handleItemClick("Hard back", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay text-[13px] 1024:text-[10px]">
                                 <p className={selectedFormat === "Hard back" ? 'text-[#B79248]' : 'text-black'}>Hard back</p>
-                                <p className="text-black text-[13px]">£0.00</p>
+                                <p className="text-black">£0.00</p>
                             </li>
-                            <li onClick={() => handleItemClick("eBook", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay">
+                            <li onClick={() => handleItemClick("eBook", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay text-[13px] 1024:text-[10px]">
                                 <p className={selectedFormat === "eBook" ? 'text-[#B79248]' : 'text-black'}>eBook</p>
-                                <p className="text-black text-[13px]">£0.00</p>
+                                <p className="text-black">£0.00</p>
                             </li>
-                            <li onClick={() => handleItemClick("Audiobook", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay">
+                            <li onClick={() => handleItemClick("Audiobook", "£0.00")} className="hover:bg-gray-100 cursor-pointer rounded flex justify-between p-3 font-fairplay text-[13px] 1024:text-[10px]">
                                 <p className={selectedFormat === "Audiobook" ? 'text-[#B79248]' : 'text-black'}>Audiobook</p>
-                                <p className="text-black text-[13px]">£0.00</p>
+                                <p className="text-black">£0.00</p>
                             </li>
                         </ul>
                     </div>
--- src/pages/wishlist/components/wishlistBooks.js
+++ src/pages/wishlist/components/wishlistBooks.js
@@ -5,10 +5,12 @@ function WishlistBooks() {
     return (
         <div className="grid 768:grid-cols-3 1024:grid-cols-6 justify-center 2560:mt-6 768:mx-2">
             {fantasyReadData.map((book, index) => (
-                <div key={index} className="relative m-4 p-3 text-center 768:m-0 1440:m-1">
-                    <div className="relative bg-cover bg-center rounded-md overflow-hidden h-[420px] 1024:h-[140px] 1024:w-[160px] 1440:h-[210px] 2560:w-[240px] 2560:h-[380px]" style={{ backgroundImage: `url(${book.imageSrc})`, width: '240px', height: '380px' }}>
-                        <div className="absolute top-0 right-0 p-2 cursor-pointer bg-white rounded-full m-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.3em" viewBox="0 0 24 24" className=''>
+                <div key={index} className="relative m-4 p-3 1024:m-1 1024:p-1 text-center 768:m-0 1440:m-1">
+                    <div className="relative bg-cover bg-center rounded-md overflow-hidden w-[240px] h-[420px] 1024:h-[180px] 1024:w-[110px] 1440:h-[210px] 2560:w-[240px] 2560:h-[380px]" style={{ backgroundImage: `url(${book.imageSrc})`, }}>
+                        {/* width: '240px', height: '380px' */}
+                        <div className="absolute top-0 right-0 p-2 1024:p-1 cursor-pointer bg-white rounded-full m-2 1024:m-1">
+                            {/* */}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.3em" viewBox="0 0 24 24" className='1024:w-3 1024:h-3'>
                                 <g fill="none" stroke="#8D8F8F" stroke-width="1.5">
                                     <path stroke-linecap="round" d="M20.5 6h-17m15.333 2.5l-.46 6.9c-.177 2.654-.265 3.981-1.13 4.79c-.865.81-2.196.81-4.856.81h-.774c-2.66 0-3.991 0-4.856-.81c-.865-.809-.954-2.136-1.13-4.79l-.46-6.9M9.5 11l.5 5m4.5-5l-.5 5" />
                                     <path d="M6.5 6h.11a2 2 0 0 0 1.83-1.32l.034-.103l.097-.291c.083-.249.125-.373.18-.479a1.5 1.5 0 0 1 1.094-.788C9.962 3 10.093 3 10.355 3h3.29c.262 0 .393 0 .51.019a1.5 1.5 0 0 1 1.094.788c.055.106.097.23.18.479l.097.291A2 2 0 0 0 17.5 6" />
@@ -17,13 +19,13 @@ function WishlistBooks() {
                         </div>
                     </div>
                     <div className='w-full text-start my-2'>
-                        <h3 className="1024:text-[8px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay ">{book.title}</h3>
-                        <p className="1024:text-[6px] 1440:text-[9px] 2560:text-[17px] text-[#B79248] font-roboto">{book.author}</p>
-                        <h3 className="1024:text-[8px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay ">£0.00</h3>
+                        <h3 className="1024:text-[9px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay ">{book.title}</h3>
+                        <p className="1024:text-[8px] 1440:text-[9px] 2560:text-[17px] text-[#B79248] font-roboto">{book.author}</p>
+                        <h3 className="1024:text-[9px] 1440:text-[11px] 2560:text-[19px] font-medium font-fairplay ">£0.00</h3>
                     </div>
                     <div className='py-4 2000:py-10'>
                         <DropdownMenu />
-                        <button className='w-full border border-[#CCCED1] h-[40px] my-5 2000:h-[54px] rounded-3xl 2000:my-10 flex items-center justify-center'>
+                        <button className='w-full border border-[#CCCED1] h-[40px] my-5 1024:h-[28px] 2000:h-[54px] rounded-3xl 2000:my-10 flex items-center justify-center'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 20 20" className="w-6 1024:w-3 1440:w-5 2560:w-7">
                                 <g transform="translate(20 0) scale(-1 1)">
                                     <g fill="currentColor">
@@ -37,7 +39,7 @@ function WishlistBooks() {
                                     </g>
                                 </g>
                             </svg>
-                            <span className="ml-3 text-[12px] 2000:text-[18px] font-fairplay items-center">Add to cart</span>
+                            <span className="ml-3 text-[12px] 1024:ml-1 1024:text-[9px] 2000:text-[18px] font-fairplay items-center">Add to cart</span>
                         </button>
                     </div>
                 </div>
